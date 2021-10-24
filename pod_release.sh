ffe88865347bf2a9e805ca1fa71a2cad3241d3b9
denglibing <denglibing3@jd.com>
2021-10-24 14:42:35 +0800

    optimze

--- pod_release.sh
+++ pod_release.sh
@@ -1,13 +1,22 @@
+function pushTagAndPodTrunk {
+	git tag -m $1 $2
+	git push orgin $2
+
+	pod trunk push HDCoverage.podspec --allow-warnings
+}
+
 function main {
 	echo "git commit msg: $1, tag: $2"
 	git add .
 	git commit -m $1
 	git push orgin master
 
-	git tag -m $1 $2
-	git push orgin $2
+	if [ $? -eq 0 ]; then
+		pushTagAndPodTrunk $1 $2
+	else
+		pushTagAndPodTrunk $1 $2
+	fi
 
-	pod trunk push HDCoverage.podspec --allow-warnings
 }
 
 main $1 $2
