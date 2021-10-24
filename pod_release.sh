80d15dbe24035f2d3cce110f7d3d90c977c1880e
denglibing <denglibing3@jd.com>
2021-10-24 14:43:58 +0800

    optimze

--- pod_release.sh
+++ pod_release.sh
@@ -7,15 +7,16 @@ function pushTagAndPodTrunk {
 
 function main {
 	echo "git commit msg: $1, tag: $2"
+
 	git add .
 	git commit -m $1
-	git push orgin master
+	git push -u orgin master
 
 	if [ $? -eq 0 ]; then
-		echo "push success"
+		echo "git push success"
 		pushTagAndPodTrunk $1 $2
 	else
-		echo "push faild"
+		echo "git push faild"
 		pushTagAndPodTrunk $1 $2
 	fi
 
