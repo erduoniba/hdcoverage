33b91d29f1f9193706fbcc194fbbc435fd44059c
denglibing <denglibing3@jd.com>
2021-10-24 14:24:38 +0800

    optimze

--- pod_release.sh
+++ pod_release.sh
@@ -3,22 +3,16 @@ function main {
 	git add .
 	git commit -m $1
 
+	echo "git commit success"
+	git tag -m $1 $2
+	git push --tags
+
 	if [ $? -eq 0 ]; then
-        
-        echo "git commit success"
-		git tag -m $1 $2
-		git push --tags
-
-		if [ $? -eq 0 ]; then
-			echo "git push tag success"
-			pod trunk push HDCoverage.podspec --allow-warnings
-		else
-			echo "git push tag faild"
-		fi
-
-    else
-        echo "git commit faild"
-    fi
+		echo "git push tag success"
+		pod trunk push HDCoverage.podspec --allow-warnings
+	else
+		echo "git push tag faild"
+	fi
 }
 
 main $1 $2
