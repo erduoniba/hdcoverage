61adf552092c10c816ceb0f62ad448071dba2913
denglibing <denglibing3@jd.com>
2021-10-24 14:54:39 +0800

    add demo project

---
+++ Example/HDCoverageDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HDCoverageDemo
+//
+//  Created by denglibing on 2021/10/24.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
