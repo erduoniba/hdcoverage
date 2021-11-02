d5bdb806f6d5a0f439320d1db3da8a4f7e9e68bf
denglibing <denglibing3@jd.com>
2021-11-02 15:33:08 +0800

    add Source-based code

--- Example/HDCoverageDemo/ViewController.swift
+++ Example/HDCoverageDemo/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+     
+        let imageView = UIImageView(frame: self.view.bounds)
+        imageView.image = UIImage(named: "2")
+        imageView.image = UIImage(named: "1")
+        imageView.image = UIImage(named: "off_demo")
+        view.addSubview(imageView)
     }
 
     
