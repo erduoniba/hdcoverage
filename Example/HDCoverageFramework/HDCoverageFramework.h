608719d3bccd54923e2b5d1378a175e36a022c67
denglibing5 <denglibing3@jd.com>
2022-04-24 11:55:24 +0800

    动态库需要单独收集

--- Example/HDCoverageFramework/HDCoverageFramework.h
+++ Example/HDCoverageFramework/HDCoverageFramework.h
@@ -17,3 +17,10 @@ FOUNDATION_EXPORT const unsigned char HDCoverageFrameworkVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <HDCoverageFramework/PublicHeader.h>
 #import "HDOCFramework.h"
 
+int __llvm_profile_runtime = 0;
+void __llvm_profile_initialize_file(void);
+const char *__llvm_profile_get_filename();
+void __llvm_profile_set_filename(const char *);
+int __llvm_profile_write_file();
+int __llvm_profile_register_write_file_atexit(void);
+const char *__llvm_profile_get_path_prefix();
