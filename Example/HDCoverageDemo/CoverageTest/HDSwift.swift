1f28693e69f394d2d57ef5378ef9dc42cbc38ad2
denglibing5 <denglibing3@jd.com>
2022-06-10 15:12:55 +0800

    block 测试有效

--- Example/HDCoverageDemo/CoverageTest/HDSwift.swift
+++ Example/HDCoverageDemo/CoverageTest/HDSwift.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct HDSwift {
     static func projectSwiftAction(_ tag: Int) {
@@ -18,5 +19,17 @@ struct HDSwift {
         else if (tag == 3) {
             print("projectSwiftAction: 3")
         }
+        
+        UIView.animate(withDuration: 0.1, delay: 0.3, options: .allowAnimatedContent) {
+            if arc4random() % 2 == 0 {
+                print("animate block 1")
+            }
+            else {
+                print("animate block 2")
+            }
+        } completion: { _ in
+            print("completion block")
+        }
+
     }
 }
